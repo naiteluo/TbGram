48f18b0dfd35d632e50408437f3fc3af1fc32222
naiteluo <naiteluo@gmail.com>
2014-06-15 23:06:22 +0800

    sticker preview

--- sticks/sticks_tabs_conf.js
+++ sticks/sticks_tabs_conf.js
@@ -7,87 +7,104 @@
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_1.png"
+                    "url": "../sticks/resource/hats/hat_1.png",
+                    "url_preview": "../sticks/resource/hats/hat_1_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_2.png"
+                    "url": "../sticks/resource/hats/hat_2.png",
+                    "url_preview": "../sticks/resource/hats/hat_2_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_3.png"
+                    "url": "../sticks/resource/hats/hat_3.png",
+                    "url_preview": "../sticks/resource/hats/hat_3_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_4.png"
+                    "url": "../sticks/resource/hats/hat_4.png",
+                    "url_preview": "../sticks/resource/hats/hat_4_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_5.png"
+                    "url": "../sticks/resource/hats/hat_5.png",
+                    "url_preview": "../sticks/resource/hats/hat_5_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_6.png"
+                    "url": "../sticks/resource/hats/hat_6.png",
+                    "url_preview": "../sticks/resource/hats/hat_6_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_7.png"
+                    "url": "../sticks/resource/hats/hat_7.png",
+                    "url_preview": "../sticks/resource/hats/hat_7_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_8.png"
+                    "url": "../sticks/resource/hats/hat_8.png",
+                    "url_preview": "../sticks/resource/hats/hat_8_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_9.png"
+                    "url": "../sticks/resource/hats/hat_9.png",
+                    "url_preview": "../sticks/resource/hats/hat_9_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_10.png"
+                    "url": "../sticks/resource/hats/hat_10.png",
+                    "url_preview": "../sticks/resource/hats/hat_10_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_11.png"
+                    "url": "../sticks/resource/hats/hat_11.png",
+                    "url_preview": "../sticks/resource/hats/hat_11_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_12.png"
+                    "url": "../sticks/resource/hats/hat_12.png",
+                    "url_preview": "../sticks/resource/hats/hat_12_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_13.png"
+                    "url": "../sticks/resource/hats/hat_13.png",
+                    "url_preview": "../sticks/resource/hats/hat_13_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/hat_14.png"
+                    "url": "../sticks/resource/hats/hat_14.png",
+                    "url_preview": "../sticks/resource/hats/hat_14_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/shangdanmao_1.png"
+                    "url": "../sticks/resource/hats/shangdanmao_1.png",
+                    "url_preview": "../sticks/resource/hats/shangdanmao_1_p_.png"
                 },
                 {
                     "name": "hat",
                     "title": "小帽子",
-                    "url": "../sticks/resource/hats/shengdanmao_2.png"
+                    "url": "../sticks/resource/hats/shengdanmao_2.png",
+                    "url_preview": "../sticks/resource/hats/shengdanmao_2_p_.png"
                 },
                 {
                     "name": "wings",
                     "title": "翅膀",
-                    "url": "../sticks/resource/hats/wings.png"
+                    "url": "../sticks/resource/hats/wings.png",
+                    "url_preview": "../sticks/resource/hats/wings_p_.png"
                 }
             ]
         },
@@ -97,42 +114,50 @@
                 {
                     "name": "glass",
                     "title": "眼镜",
-                    "url": "../sticks/resource/glass/glass_1.png"
+                    "url": "../sticks/resource/glass/glass_1.png",
+                    "url_preview": "../sticks/resource/glass/glass_1_p_.png"
                 },
                 {
                     "name": "glass",
                     "title": "眼镜",
-                    "url": "../sticks/resource/glass/glass_2.png"
+                    "url": "../sticks/resource/glass/glass_2.png",
+                    "url_preview": "../sticks/resource/glass/glass_2_p_.png"
                 },
                 {
                     "name": "glass",
                     "title": "眼镜",
-                    "url": "../sticks/resource/glass/glass_3.png"
+                    "url": "../sticks/resource/glass/glass_3.png",
+                    "url_preview": "../sticks/resource/glass/glass_3_p_.png"
                 },
                 {
                     "name": "glass",
                     "title": "眼镜",
-                    "url": "../sticks/resource/glass/glass_4.png"
+                    "url": "../sticks/resource/glass/glass_4.png",
+                    "url_preview": "../sticks/resource/glass/glass_4_p_.png"
                 },
                 {
                     "name": "glass",
                     "title": "眼镜",
-                    "url": "../sticks/resource/glass/glass_5.png"
+                    "url": "../sticks/resource/glass/glass_5.png",
+                    "url_preview": "../sticks/resource/glass/glass_5_p_.png"
                 },
                 {
                     "name": "glass",
                     "title": "眼镜",
-                    "url": "../sticks/resource/glass/glass_6.png"
+                    "url": "../sticks/resource/glass/glass_6.png",
+                    "url_preview": "../sticks/resource/glass/glass_6_p_.png"
                 },
                 {
                     "name": "glass",
                     "title": "眼镜",
-                    "url": "../sticks/resource/glass/glass_7.png"
+                    "url": "../sticks/resource/glass/glass_7.png",
+                    "url_preview": "../sticks/resource/glass/glass_7_p_.png"
                 },
                 {
                     "name": "glass",
                     "title": "眼镜",
-                    "url": "../sticks/resource/glass/glass_8.png"
+                    "url": "../sticks/resource/glass/glass_8.png",
+                    "url_preview": "../sticks/resource/glass/glass_8_p_.png"
                 }
             ]
         },
@@ -142,82 +167,98 @@
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1005.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1005.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1005_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1006.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1006.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1006_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1007.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1007.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1007_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1008.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1008.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1008_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1009.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1009.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1009_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1010.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1010.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1010_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1011.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1011.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1011_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1012.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1012.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1012_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1013.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1013.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1013_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1014.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1014.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1014_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1021.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1021.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1021_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1022.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1022.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1022_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1023.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1023.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1023_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1024.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1024.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1024_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1025.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1025.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1025_p_.png"
                 },
                 {
                     "name": "cartoon",
                     "title": "卡通",
-                    "url": "../sticks/resource/cartoon/Madagascar_1026.png"
+                    "url": "../sticks/resource/cartoon/Madagascar_1026.png",
+                    "url_preview": "../sticks/resource/cartoon/Madagascar_1026_p_.png"
                 }
             ]
         }
